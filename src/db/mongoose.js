3c1fd251c540bab4904127b96142e165609dd835
Diego Valadez <the-vala@users.noreply.github.com>
2019-11-25 17:13:56 -0600

    Resolved connectionUsl setting bug

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // revisa tu connectionURL aqui :-)
-var connectionURL = process.env.connectionURL || require('../config.js').connectionURL
+var connectionURL = process.env.DB_CONNECTION_STRING || require('../config.js').connectionURL
 
 mongoose.connect( connectionURL, {
   useNewUrlParser: true,
